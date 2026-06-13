06bba5ced6b47ad6127f8f83e0e464bccec71688
nrparajuli <144822375+nrparajuli@users.noreply.github.com>
2026-06-13 14:20:52 +0545

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (!navbar || !toggle) return;
 
-  toggle.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    // toggle mobile menu
+  toggle.addEventListener("click", function () {
     navbar.classList.toggle("navbar-mobile");
 
-    // toggle icon
     this.classList.toggle("bi-list");
     this.classList.toggle("bi-x");
   });
 
-  // close menu when clicking any link
   document.querySelectorAll("#navbar a").forEach(link => {
     link.addEventListener("click", () => {
       navbar.classList.remove("navbar-mobile");
-
       toggle.classList.add("bi-list");
       toggle.classList.remove("bi-x");
     });
