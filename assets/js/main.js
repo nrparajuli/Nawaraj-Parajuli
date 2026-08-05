eb6d687f9a3d4c1050005d6b4c6bc0b3d52ce73a
nrparajuli <144822375+nrparajuli@users.noreply.github.com>
2026-08-05 21:45:24 +0545

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-alert("main.js loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const navbar = document.querySelector("#navbar");
+  const toggle = document.querySelector(".mobile-nav-toggle");
+
+  if (!navbar || !toggle) return;
+
+  toggle.addEventListener("click", function () {
+    navbar.classList.toggle("navbar-mobile");
+
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  document.querySelectorAll("#navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+      navbar.classList.remove("navbar-mobile");
+      toggle.classList.add("bi-list");
+      toggle.classList.remove("bi-x");
+    });
+  });
+
+});
